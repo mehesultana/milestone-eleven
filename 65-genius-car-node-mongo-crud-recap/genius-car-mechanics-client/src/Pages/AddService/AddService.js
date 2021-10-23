2ad21ac621d1702bcbe8372789f3f90670a25641
mehesultana <sultanamehe01@gmail.com>
2021-10-24 02:21:46 +0600

    setup the add service form

--- 65-genius-car-node-mongo-crud-recap/genius-car-mechanics-client/src/Pages/AddService/AddService.js
+++ 65-genius-car-node-mongo-crud-recap/genius-car-mechanics-client/src/Pages/AddService/AddService.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import './AddService.css';
 
 const AddService = () => {
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
-		<div>
+		<div className="add-service">
 			<h2>add a service</h2>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input {...register('name', { required: true, maxLength: 20 })} placeholder="Name" />
+				<input {...register('description')} placeholder="Description" />
+				<input type="number" {...register('price')} placeholder="Price" />
+				<input {...register('img')} placeholder="Img" />
+				<input type="submit" />
+			</form>
 		</div>
 	);
 };
