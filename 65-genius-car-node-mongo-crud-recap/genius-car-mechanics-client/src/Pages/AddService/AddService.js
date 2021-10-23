5cc307d756f8d35aa0e30dc03faaf4cb8e1f0644
mehesultana <sultanamehe01@gmail.com>
2021-10-24 03:11:24 +0600

    Load all services and single service from the server

--- 65-genius-car-node-mongo-crud-recap/genius-car-mechanics-client/src/Pages/AddService/AddService.js
+++ 65-genius-car-node-mongo-crud-recap/genius-car-mechanics-client/src/Pages/AddService/AddService.js
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import './AddService.css';
 
 const AddService = () => {
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, reset } = useForm();
 
 	const onSubmit = (data) => {
 		console.log(data);
@@ -12,6 +12,7 @@ const AddService = () => {
 		axios.post('http://localhost:5000/services', data).then((res) => {
 			if (res.data.insertedId) {
 				alert('added succesfully');
+				reset();
 			}
 		});
 	};
