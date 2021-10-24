2cc89670e03480475cd5549956d733659b775f0e
mehesultana <sultanamehe01@gmail.com>
2021-10-24 19:52:24 +0600

    Create Post api and UI page to insert service

--- 65-genius-car-node-mongo-crud/genius-car-mechanic-server/index.js
+++ 65-genius-car-node-mongo-crud/genius-car-mechanic-server/index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
 
-const uri = 'mongodb+srv://<username>:<password>@cluster0.b4chv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4chv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+	try {
+		await client.connect();
+		// console.log('connected to database');
+
+		const database = client.db('geniusMechanic');
+		const servicesCollection = database.collection('services');
+
+		// post API
+		app.post('/services', async (req, res) => {
+			const service = req.body;
+
+			console.log('hit the post api', service);
+
+			// const service = {
+			// 	name: 'ENGINE DIAGNOSTIC',
+			// 	price: '300',
+			// 	description: 'Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.',
+			// 	img: 'https://i.ibb.co/dGDkr4v/1.jpg',
+			// };
+			const result = await servicesCollection.insertOne(service);
+			console.log(result);
+			res.json(result);
+		});
+	} finally {
+		// await client.close()
+	}
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('running server');
