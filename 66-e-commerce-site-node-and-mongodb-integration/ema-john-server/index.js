a0e07df7c404e01e4afde89772e7bffe3032e1c9
mehesultana <sultanamehe01@gmail.com>
2021-10-25 20:21:55 +0600

    gitignore

--- 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
+++ 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
@@ -25,7 +25,7 @@ async function run() {
 		//get products API
 		app.get('/products', async (req, res) => {
 			const cursor = productCollection.find({});
-			const products = await cursor.toArray();
+			const products = await cursor.limit(10).toArray();
 			res.send(products);
 		});
 	} finally {
