1ccdd90b27be0d3aaa530de2f081cd9cb484ae7c
mehesultana <sultanamehe01@gmail.com>
2021-10-26 00:15:03 +0600

    Use Post to load some products using keys

--- 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
+++ 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
@@ -44,6 +44,14 @@ async function run() {
 				products,
 			});
 		});
+
+		// Use POST to get data by keys
+		app.post('/products/byKeys', async (req, res) => {
+			const keys = req.body;
+			const query = { key: { $in: keys } };
+			const products = await productCollection.find(query).toArray();
+			res.json(products);
+		});
 	} finally {
 		// await client.close()
 	}
