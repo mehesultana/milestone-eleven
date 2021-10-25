577d6543927719501e1d8eddcef6f3c8e0a9f282
mehesultana <sultanamehe01@gmail.com>
2021-10-26 02:11:43 +0600

    Save orders in a new data collection in mongodb

--- 66-e-commerce-site-node-and-mongodb-integration/ema-john-client/src/hooks/useCart.js
+++ 66-e-commerce-site-node-and-mongodb-integration/ema-john-client/src/hooks/useCart.js
@@ -1,13 +1,11 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getStoredCart } from '../utilities/fakedb';
 
 const useCart = () => {
 	const [cart, setCart] = useState([]);
+
 	useEffect(() => {
 		const savedCart = getStoredCart();
-
-		// console.log(savedCart);
 		const keys = Object.keys(savedCart);
 		fetch('http://localhost:5000/products/byKeys', {
 			method: 'POST',
@@ -18,25 +16,23 @@ const useCart = () => {
 		})
 			.then((res) => res.json())
 			.then((products) => {
-				console.log(products);
-
-				// if (products.length) {
-
-				// 	const storedCart = [];
-				// 	for (const key in savedCart) {
-				// 		const addedProduct = products.find((product) => product.key === key);
-				// 		if (addedProduct) {
-				// 			//set quantity
-				// 			const quantity = savedCart[key];
-				// 			addedProduct.quantity = quantity;
-				// 			storedCart.push(addedProduct);
-				// 		}
-				// 	}
-				// 	setCart(storedCart);
-				// }
+				if (products.length) {
+					const storedCart = [];
+					for (const key in savedCart) {
+						const addedProduct = products.find((product) => product.key === key);
+						if (addedProduct) {
+							// set quantity
+							const quantity = savedCart[key];
+							addedProduct.quantity = quantity;
+							storedCart.push(addedProduct);
+						}
+					}
+					setCart(storedCart);
+				}
 			});
 	}, []);
 
 	return [cart, setCart];
 };
+
 export default useCart;
