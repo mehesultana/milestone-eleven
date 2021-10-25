e16dbf5ae04ca38a83fffd154959a90666d0a281
mehesultana <sultanamehe01@gmail.com>
2021-10-25 19:51:03 +0600

    Run client side project and connect with mongo database

--- 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
+++ 66-e-commerce-site-node-and-mongodb-integration/ema-john-server/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4chv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+
+async function run() {
+	try {
+		await client.connect();
+		console.log('database connected');
+	} finally {
+		// await client.close()
+	}
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Ema john is running');
