50def2438272c557c0cb1991d4f9113a56185207
mehesultana <sultanamehe01@gmail.com>
2021-11-01 20:28:56 +0600

    project setup

---
+++ 68.5-jwt-token-and-image-hosting/ema-john-client-2/src/components/Shop/Shop.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import useCart from '../../hooks/useCart';
+import { Link } from 'react-router-dom';
+
+const Shop = () => {
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useCart();
+	const [page, setPage] = useState(0);
+	const [pageCount, setPageCount] = useState(0);
+	const [displayProducts, setDisplayProducts] = useState([]);
+	const size = 10;
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setProducts(data.products);
+				setDisplayProducts(data.products);
+				const count = data.count;
+				const pageNumber = Math.ceil(count / size);
+				setPageCount(pageNumber);
+			});
+	}, [page]);
+
+	useEffect(() => {
+		// console.log('local storage callled');
+		if (products.length) {
+			const savedCart = getStoredCart();
+			// console.log(savedCart);
+			const storedCart = [];
+			for (const key in savedCart) {
+				// console.log(key);
+
+				const addedProduct = products.find((product) => product.key === key);
+				// console.log(addedProduct);
+				if (addedProduct) {
+					const quantity = savedCart[key];
+					addedProduct.quantity = quantity;
+					// console.log(addedProduct);
+					storedCart.push(addedProduct);
+				}
+			}
+			setCart(storedCart);
+		}
+	}, [products]);
+
+	const handleAddToCart = (product) => {
+		const exists = cart.find((pd) => pd.key === product.key);
+		let newCart = [];
+		if (exists) {
+			const rest = cart.filter((pd) => pd.key !== product.key);
+			exists.quantity = exists.quantity + 1;
+			newCart = [...rest, product];
+		} else {
+			product.quantity = 1;
+			newCart = [...cart, product];
+		}
+
+		setCart(newCart);
+		//save to local storage (for now)
+		addToDb(product.key);
+	};
+
+	const handleSearch = (event) => {
+		const searchText = event.target.value;
+		const matchedProducts = products.filter((product) => product.name.toLowerCase().includes(searchText.toLowerCase()));
+		setDisplayProducts(matchedProducts);
+		console.log(matchedProducts);
+	};
+
+	return (
+		<div>
+			<div className="search-container">
+				<input type="text" onChange={handleSearch} placeholder="search product" />
+			</div>
+			<div className="shop-container">
+				<div className="product-container">
+					{displayProducts.map((product) => (
+						<Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>
+					))}
+					<div className="pagination">
+						{[...Array(pageCount).keys()].map((number) => (
+							<button className={number === page ? 'selected' : ''} key={number} onClick={() => setPage(number)}>
+								{number + 1}
+							</button>
+						))}
+					</div>
+				</div>
+				<div className="cart-container">
+					<Cart cart={cart}>
+						<Link to="/review">
+							<button className="btn-regular">Review Your Order</button>
+						</Link>
+					</Cart>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Shop;
